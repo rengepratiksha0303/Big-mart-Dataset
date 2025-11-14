48aed0a2c4924b6660096068d3eb8533d0822bb0
rengepratiksha0303 <rengepratiksha0303@gmail.com>
2025-11-14 17:37:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import pickle
 
 # === Load Model and Version Info from Pickle ===
-with open("bigmart_best_model.pkl", "rb") as f:
+with open("bigmart_best_model.pkl") as f:
     model, sklearn_version = pickle.load(f)
 
 st.title("🛒 BigMart Sales Prediction App")
